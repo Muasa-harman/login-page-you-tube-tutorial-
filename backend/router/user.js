3f86009043e896d13180d35b649cdf71339bf06f
Muasa-harman <harman.muasa@gmail.com>
2024-05-13 10:17:26 +0300

    routes updated

--- backend/router/user.js
+++ backend/router/user.js
@@ -6,6 +6,6 @@ import { Register, signin } from "../controller/user.js";
 const router = express.Router()
 
 router.post("/register",Register);
-router.post("/login",signin)
+router.post("/login",signin);
 
 export default router;
